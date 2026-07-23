a74408f38470c2457da6fe1093a9e2510a1053f1
Artur Rios <arturdev@duck.com>
2026-07-23 09:43:15 -0300

    feat: add health check endpoint

---
+++ src/Presentation/ArturRios.IdentityManager.WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,22 @@
+﻿using ArturRios.Output;
+using ArturRios.Util.Http;
+using ArturRios.Util.WebApi.AspNetCore;
+using ArturRios.Util.WebApi.Security.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArturRios.IdentityManager.WebApi.Controllers;
+
+public class HealthCheckController : Controller
+{
+    [HttpGet]
+    [Route("")]
+    [AllowAnonymous]
+    public ActionResult<DataOutput<string?>> HelloWorld()
+    {
+        var result = DataOutput<string?>.New
+            .WithData("Hello world!")
+            .WithMessage("Identity manager API is working.");
+
+        return ResponseResolver.Resolve(result, HttpStatusCodes.Ok);
+    }
+}
